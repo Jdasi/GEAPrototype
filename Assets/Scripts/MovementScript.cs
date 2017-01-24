fd776781866efcaffef83457438b646e23dc05c6
Joe da Silva <silvacore@hotmail.co.uk>
2017-01-24 15:39:23 +0000

    added back latest movement script

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,61 +1,73 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MovementScript : MonoBehaviour
 {
-	public float speed = 1.0f;
-	public string axisName = "Horizontal";
-	public Animator anim;
-	public bool OnGround;
+    public float speed = 1.0f;
+    public string axisName = "Horizontal";
+    public Animator anim;
+    public bool OnGround;
 
-	// Use this for initialization
-	void Start () 
-	{
-		anim = gameObject.GetComponent<Animator> ();
-	}
+    public bool on_rope = false;
+    private GameObject parented_rope;
 
-	void Update()
-	{  
+    private Rigidbody2D rigid_body;
 
-	}
+    void Start() 
+    {
+        anim = GetComponent<Animator>();
+        rigid_body = GetComponent<Rigidbody2D>();
+    }
 
-	void OnCollisionStay2D(Collision2D coll)
-	{
-		OnGround = true;
-		if (OnGround == true)
-		{
-			if (Input.GetKeyDown (KeyCode.Space)) 
-			{  //makes player jump
-				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 3), ForceMode2D.Impulse);
-			}
-		}
-	}
+    void Update()
+    {  
+        if (parented_rope)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, parented_rope.transform.position, 1);
+            parented_rope.GetComponent<Rigidbody2D>().AddForce(new Vector2(5 * Input.GetAxis(axisName), 0));
+        }
+        else
+        {
+            transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
+        }
+    }
 
-	void OnCollisionExit2D(Collision2D coll)
-	{
-		if (OnGround) 
-		{
-			OnGround = false;
-		}
-	}
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        OnGround = true;
 
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		if (Input.GetAxis (axisName) < 0)
-		{
-			Vector3 newScale = transform.localScale;
-			newScale.y = 1.0f;
-			newScale.x = 1.0f;
-			transform.localScale = newScale;
-		} 
-		else if (Input.GetAxis (axisName) > 0)
-		{
-			Vector3 newScale =transform.localScale;
-			newScale.x = 1.0f;
-			transform.localScale = newScale;        
-		}
+        if (OnGround == true)
+        {
+            if (Input.GetKeyDown (KeyCode.Space)) 
+            {  //makes player jump
+                GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
 
-		transform.position += transform.right *Input.GetAxis(axisName)* speed * Time.deltaTime;
-	}
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (OnGround) 
+        {
+            OnGround = false;
+        }
+    }
+
+    public void setParentedRope(GameObject rope)
+    {
+        if (rope)
+        {
+            parented_rope = rope;
+            transform.SetParent(rope.transform);
+            rigid_body.gravityScale = 0;
+            on_rope = true;
+        }
+        else
+        {
+            parented_rope = rope;
+            transform.SetParent(rope.transform);
+            rigid_body.gravityScale = 0;
+            on_rope = false;
+        }
+    }
 }
