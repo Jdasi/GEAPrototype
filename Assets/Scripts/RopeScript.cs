55b2b79f23854bf24b3f4e49b02aeb0078997b8f
Joseph da Silva <joseph2.dasilva@live.uwe.ac.uk>
2017-01-24 11:48:04 +0000

    Working on rope script

---
+++ Assets/Scripts/RopeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeScript : MonoBehaviour
+{
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        other.transform.SetParent(gameObject.transform);
+    }
+}
