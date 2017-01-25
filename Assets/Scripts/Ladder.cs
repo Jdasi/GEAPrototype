dcf192c5989d46c8c763c801fe292a5a10768432
Joe da Silva <silvacore@hotmail.co.uk>
2017-01-25 16:38:53 +0000

    Added basic ladder climbing (you can't go DOWN through 1 way platforms yet)

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ladder : MonoBehaviour
+{
+    MovementScript player;
+
+	void Start()
+    {
+	    player = GameObject.FindGameObjectWithTag("Player").GetComponent<MovementScript>();
+	}
+	
+	void Update()
+    {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        player.ladder_in_reach = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        player.ladder_in_reach = false;
+    }
+}
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class MovementScript : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public string axisName = "Horizontal";
+    public float walk_speed = 1.0f;
+    public float climb_speed = 1.0f;
+
+    public string horizontal_axis = "Horizontal";
+    public string vertical_axis = "Vertical";
+
     public Animator anim;
     public bool OnGround;
 
     public bool on_rope = false;
     private GameObject parented_rope;
+
+    public bool ladder_in_reach = false;
+    private bool on_ladder = false;
 
     private Rigidbody2D rigid_body;
 
@@ -25,11 +32,33 @@ public class MovementScript : MonoBehaviour
         if (parented_rope)
         {
             transform.position = Vector3.MoveTowards(transform.position, parented_rope.transform.position, 1);
-            parented_rope.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f * Input.GetAxis(axisName), 0));
+            parented_rope.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f * Input.GetAxis(horizontal_axis), 0));
         }
         else
         {
-            transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
+            transform.position += transform.right * Input.GetAxis(horizontal_axis) * walk_speed * Time.deltaTime;
+        }
+
+        if (ladder_in_reach)
+        {
+            if (Input.GetAxis(vertical_axis) != 0)
+            {
+                on_ladder = true;
+            }
+        }
+        else
+        {
+            on_ladder = false;
+        }
+
+        if (on_ladder)
+        {
+            rigid_body.gravityScale = 0;
+            transform.position += transform.up * Input.GetAxis(vertical_axis) * climb_speed * Time.deltaTime;
+        }
+        else
+        {
+            rigid_body.gravityScale = 1;
         }
 
     }
