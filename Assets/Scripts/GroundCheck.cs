9f3b390734a335c61a7b1d1a18b619d9faa013e9
Joe da Silva <silvacore@hotmail.co.uk>
2017-01-25 21:14:06 +0000

    Bug fix to player jumping when moving between blocks

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -15,7 +15,7 @@ public class GroundCheck : MonoBehaviour
 	
 	}
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.tag != "Ground")
             return;
