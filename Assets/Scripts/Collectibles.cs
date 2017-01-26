d2032523b38cd479f91e1674b4f2e28be992be3b
mitchell <mitchell.height@gmail.com>
2017-01-26 12:17:32 +0000

    HUD

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Collectibles : MonoBehaviour {
+    // Use this for initialization
+
+    private int collectibles, hundreds, tens, units;
+    public Text collectiblesText;
+	void Start () {
+        collectibles = 0;
+        hundreds = 0;
+        tens = 0;
+        units = 0;
+        updateCountText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (gameObject)
+            {
+                Destroy(gameObject);
+                collectibles++;
+                units++;
+                if (units > 9)
+                {
+                    units = 0;
+                    tens++;
+                    if(tens > 9)
+                    {
+                        tens = 0;
+                        hundreds++;
+                    }
+                }
+                updateCountText();
+            }
+        }
+    }
+
+    void updateCountText()
+    {
+        collectiblesText.color = Color.white;
+        collectiblesText.text = "Items collected " + hundreds.ToString() + tens.ToString() + units.ToString();
+    }
+   
+}
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+    public int hours;
+    public int minutesTens;
+    public int minutes;
+    public Text time;
+    public float elapsed;
+    // Use this for initialization
+    void Start () {
+        hours = 7;
+        minutesTens = 0;
+        minutes = 0;
+        displayTime();
+        elapsed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsed += (Time.deltaTime);
+        if(elapsed >= 30)
+        {
+
+                minutes++;
+                elapsed = 0;
+            if (minutes == 10)
+            {
+                minutes = 0;
+                minutesTens++;
+            }
+            if(minutesTens == 6)
+            {
+                minutesTens = 0;
+                hours++;
+            }
+        }
+        displayTime();
+    }
+
+    void OnGUi()
+    {
+    }
+
+    void displayTime()
+    {
+        time.text = "Time " + hours.ToString() + ":" + minutesTens.ToString() + minutes.ToString() + "am";
+    }
+}
