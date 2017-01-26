00c5fb66dc3de2c404cd6562b21dec01a689c615
Joe da Silva <silvacore@hotmail.co.uk>
2017-01-26 16:19:06 +0000

    Added nick's work

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+
+	public void SceneLoad (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
